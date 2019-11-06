21617a325952df34093a9f364cec5becaa87c2bd
Timo Schneider <schnti@users.noreply.github.com>
2019-11-06 14:08:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $blueprints = [];
 foreach (kirby()->blueprints() as $blueprint) {
-	if(Str::startsWith($blueprint, "module.")) $blueprints[] = $blueprint;
+	if(Str::startsWith($blueprint, 'module.')) $blueprints[] = $blueprint;
 }
 
 Kirby::plugin('medienbaecker/modulehelper', [
@@ -13,5 +13,18 @@ Kirby::plugin('medienbaecker/modulehelper', [
 		'modulehelper/changeTemplate' => [
 			'changeTemplate' => $blueprints
 		]
+	], 'pageMethods' => [
+		'isModule'          => function () {
+			return Str::startsWith($this->intendedTemplate(), 'module.')
+		},
+		'moduleName'        => function () {
+			return str_replace('.', '__', $this->intendedTemplate());
+		},
+		'moduleDisplayName' => function () {
+			if ($this->isModule())
+				return ucwords(str_replace('-', ' ', substr($this->intendedTemplate(), 7)));
+			else
+				return ucwords($this->intendedTemplate());
+		}
 	]
 ]);
