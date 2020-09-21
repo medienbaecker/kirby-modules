55ab2d5f8a04b5d5f316a8b531f1878ca14eadb9
Pedro Borges <oi@pedroborg.es>
2020-09-20 23:27:53 -0300

    Add "autopublish" option
    Closes #7

--- lib/models.php
+++ lib/models.php
@@ -1,6 +1,13 @@
 <?php
 
 class ModulePage extends Page {
+	public static function create(array $props) {
+		if (option('medienbaecker.modules.autopublish', true)) {
+			$props['num'] = 9999;
+		}
+		
+		return parent::create($props);
+	}
 	public function url($options = null): string {
 		return $this->parents()->filterBy('intendedTemplate', '!=', 'modules')->first()->url() . '#' . $this->slug();
 	}
