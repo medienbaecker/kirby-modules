b77cad5177e097f495afc2aab364328962b4aa52
Thomas Günther <mail@medienbaecker.com>
2020-09-23 14:39:00 +0200

    Add legacy modules to registry

--- lib/functions.php
+++ lib/functions.php
@@ -3,7 +3,10 @@
 
 function createModuleRegistry() {
 
+	// Set up registry array
 	$registry = ['blueprints' => [], 'templates' => [], 'pageModels' => []];
+	
+	// Add modules in site/modules
 	$modulesFolder = kirby()->root('site') . "/modules";
 	foreach (Dir::dirs($modulesFolder) as $folder) {
 		$blueprint = $modulesFolder . "/". $folder . "/" . $folder . ".yml";
@@ -21,6 +24,39 @@ function createModuleRegistry() {
 			$registry['pageModels']['module.'. $folder] = option('medienbaecker.modules.model', 'ModulePage');
 		}
 	}
+	
+	// Add legacy modules to registry
+	$moduleBlueprints = array_filter(kirby()->blueprints(), function($blueprint) {
+		return Str::startsWith($blueprint, 'module.');
+	});
+	if(!empty($moduleBlueprints)) {
+		$blueprintsFolder = kirby()->root('blueprints');
+		$snippetsFolder = kirby()->root('snippets');
+		foreach($moduleBlueprints as $moduleBlueprint) {
+			$blueprint = $blueprintsFolder . "/pages/" . $moduleBlueprint . ".yml";
+			$template = $snippetsFolder . "/modules/" . $moduleBlueprint . ".php";
+			if(F::exists($blueprint) AND F::exists($template)) {
+				$blueprintArray = Yaml::read($blueprint);
+				if(!array_key_exists('status', $blueprintArray)) {
+					$blueprintArray['status'] = [
+						'draft' => true,
+						'listed' => true,
+					];
+				}
+				if(!array_key_exists('pages/'. $moduleBlueprint, $registry['blueprints'])) {
+					$registry['blueprints']['pages/'. $moduleBlueprint] = $blueprintArray;	
+				}
+				if(!array_key_exists($moduleBlueprint, $registry['templates'])) {
+					$registry['templates'][$moduleBlueprint] = $template;
+				}
+				if(!array_key_exists($moduleBlueprint, $registry['pageModels'])) {
+					$registry['pageModels'][$moduleBlueprint] = option('medienbaecker.modules.model', 'ModulePage');
+				}
+			}
+		}
+	}
+	
+	// Add modules container blueprint with redirect field
 	$registry['blueprints']['pages/modules'] = [
 		'title' => 'Modules',
 		'options' => [
@@ -32,6 +68,10 @@ function createModuleRegistry() {
 			'modules_redirect' => true
 		]
 	];
+	
+	// Add modules container model
 	$registry['pageModels']['modules'] = 'ModulesPage';
+	
+	
 	return $registry;
 }
