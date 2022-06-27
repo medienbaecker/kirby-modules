095a87870d4b216d12c060de28e5eff7dc1d9c7c
Nils Hörrmann <nils.hoerrmann@hananils.de>
2022-06-27 18:11:53 +0200

    Pass section layout to create dialog
    This will allow plugins to disable redirection to the created module page (required for inline modules)

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 panel.plugin('medienbaecker/modules', {
-	components: {
-		'k-modules-section': {
-			extends: 'k-pages-section',
-			created: function () {
-				if (this.parent == 'site') return;
-				this.$api.post(this.parent + '/modules')
-					.then((data) => {
-						if (data.created) {
-							this.reload();
-						}
-					});
-			},
-			updated: function () {
-				this.$nextTick(function () {
-					this.$el.classList.add('k-modules-section');
-				})
-			}
-		},
-	},
-	fields: {
-		modules_redirect: {
-			props: {
-				redirect: String
-			},
-			render: function() {
-				window.location.href = this.redirect;
-			}
-		}
-	}
+  components: {
+    'k-modules-section': {
+      extends: 'k-pages-section',
+      created: function () {
+        if (this.parent == 'site') return;
+        this.$api.post(this.parent + '/modules').then((data) => {
+          if (data.created) {
+            this.reload();
+          }
+        });
+      },
+      computed: {
+        type() {
+          return 'modules';
+        }
+      },
+      methods: {
+        onAdd() {
+          if (this.canAdd) {
+            this.$dialog('pages/create', {
+              query: {
+                parent: this.options.link || this.parent,
+                view: this.parent,
+                section: this.name,
+                modules: this.options.layout
+              }
+            });
+          }
+        }
+      }
+    }
+  },
+  fields: {
+    modules_redirect: {
+      props: {
+        redirect: String
+      },
+      render: function () {
+        window.location.href = this.redirect;
+      }
+    }
+  }
 });
