40759f0fbd35ce674a1a0aa550ac48529c5b8ee2
Thomas Günther <mail@medienbaecker.com>
2023-01-23 13:26:02 +0100

    New registry for modules in /blueprints/modules

--- lib/functions.php
+++ lib/functions.php
@@ -1,73 +1,60 @@
 <?php
 
+use Kirby\Filesystem\Dir;
+use Kirby\Filesystem\F;
+use Kirby\Toolkit\Str;
+use Kirby\Data\Yaml;
+
 function createModuleRegistry() {
 
-  // Set up registry array
   $registry = ['blueprints' => [], 'templates' => [], 'pageModels' => []];
-  
-  // Add modules in site/modules
+
+  $blueprintsFolder = kirby()->root('blueprints');
+  $snippetsFolder = kirby()->root('snippets');
   $modulesFolder = kirby()->root('site') . "/modules";
+
+  // ----------------------------------------------------------------------
+  // Register modules in site/modules
+  // ----------------------------------------------------------------------
+
   foreach (Dir::dirs($modulesFolder) as $folder) {
-    $blueprint = $modulesFolder . "/". $folder . "/" . $folder . ".yml";
-    $template = $modulesFolder . "/". $folder . "/" . $folder . ".php";
-    if(F::exists($blueprint)) {
-      $blueprintArray = Yaml::read($blueprint);
-      if(!array_key_exists('status', $blueprintArray)) {
-        $blueprintArray['status'] = [
-          'draft' => true,
-          'listed' => true,
-        ];
-      }
-      if(!array_key_exists('navigation', $blueprintArray)) {
-        $blueprintArray['navigation'] = [
-          'status' => 'all',
-          'template' => 'all',
-        ];
-      }
-      $registry['blueprints']['pages/module.'. $folder] = $blueprintArray;
-      $registry['templates']['module.'. $folder] = $template;
-      $registry['pageModels']['module.'. $folder] = option('medienbaecker.modules.model', 'ModulePage');
-    }
+    $blueprintPath = $modulesFolder . "/". $folder . "/" . $folder . ".yml";
+    $templatePath = $modulesFolder . "/". $folder . "/" . $folder . ".php";
+    $registry = addToModulesRegistry($registry, $folder, $blueprintPath, $templatePath);
   }
-  
-  // Add legacy modules to registry
+
+  // ----------------------------------------------------------------------
+  // Register modules in site/blueprints/modules and site/snippets/modules
+  // ----------------------------------------------------------------------
+
+  $moduleBlueprintsFolder = kirby()->root('blueprints') . "/modules";
+  $moduleTemplatesFolder = kirby()->root('snippets') . "/modules";
+
+  foreach (Dir::files($moduleBlueprintsFolder) as $file) {
+    $filename = F::name($file);
+    $blueprintPath = $moduleBlueprintsFolder . "/" . $filename . ".yml";
+    $templatePath = $moduleTemplatesFolder . "/" . $filename . ".php";
+    $registry = addToModulesRegistry($registry, $filename, $blueprintPath, $templatePath);
+  }
+
+  // ----------------------------------------------------------------------
+  // Legacy registry (deprecated)
+  // ----------------------------------------------------------------------
+
   $moduleBlueprints = array_filter(kirby()->blueprints(), function($blueprint) {
     return Str::startsWith($blueprint, 'module.');
   });
-  if(!empty($moduleBlueprints)) {
-    $blueprintsFolder = kirby()->root('blueprints');
-    $snippetsFolder = kirby()->root('snippets');
-    foreach($moduleBlueprints as $moduleBlueprint) {
-      $blueprint = $blueprintsFolder . "/pages/" . $moduleBlueprint . ".yml";
-      $template = $snippetsFolder . "/modules/" . $moduleBlueprint . ".php";
-      if(F::exists($blueprint)) {
-        $blueprintArray = Yaml::read($blueprint);
-        if(!array_key_exists('status', $blueprintArray)) {
-          $blueprintArray['status'] = [
-            'draft' => true,
-            'listed' => true,
-          ];
-        }
-        if(!array_key_exists('navigation', $blueprintArray)) {
-          $blueprintArray['navigation'] = [
-            'status' => 'all',
-            'template' => 'all',
-          ];
-        }
-        if(!array_key_exists('pages/'. $moduleBlueprint, $registry['blueprints'])) {
-          $registry['blueprints']['pages/'. $moduleBlueprint] = $blueprintArray;	
-        }
-        if(!array_key_exists($moduleBlueprint, $registry['templates'])) {
-          $registry['templates'][$moduleBlueprint] = $template;
-        }
-        if(!array_key_exists($moduleBlueprint, $registry['pageModels'])) {
-          $registry['pageModels'][$moduleBlueprint] = option('medienbaecker.modules.model', 'ModulePage');
-        }
-      }
-    }
+
+  foreach($moduleBlueprints as $moduleBlueprint) {
+    $blueprintPath = $blueprintsFolder . "/pages/" . $moduleBlueprint . ".yml";
+    $templatePath = $snippetsFolder . "/modules/" . $moduleBlueprint . ".php";
+    $registry = addToModulesRegistry($registry, $moduleBlueprint, $blueprintPath, $templatePath);
   }
-  
+
+  // ----------------------------------------------------------------------
   // Add modules container blueprint with redirect field
+  // ----------------------------------------------------------------------
+  
   $registry['blueprints']['pages/modules'] = [
     'title' => 'Modules',
     'options' => [
@@ -80,9 +67,48 @@ function createModuleRegistry() {
     ]
   ];
   
+  // ----------------------------------------------------------------------
   // Add modules container model
+  // ----------------------------------------------------------------------
+
   $registry['pageModels']['modules'] = 'ModulesPage';
-  
-  
+
   return $registry;
+
+}
+
+function addToModulesRegistry(array $registry, string $name, string $blueprintPath, string $templatePath) {
+
+  // Check if blueprint already exists in registry
+  if(array_key_exists('pages/module.'. $name, $registry['blueprints'])) {
+    return;
+  }
+
+  // Check if blueprint exists
+  if(!F::exists($blueprintPath)) {
+    return;
+  }
+
+  // Turn the blueprint into an array
+  $blueprintArray = Yaml::read($blueprintPath);
+
+  // Set up default values for status and navigation
+  if(!array_key_exists('status', $blueprintArray)) {
+    $blueprintArray['status'] = ['draft' => true, 'listed' => true];
+  }
+  if(!array_key_exists('navigation', $blueprintArray)) {
+    $blueprintArray['navigation'] = ['status' => 'all', 'template' => 'all'];
+  }
+
+  if(Str::startsWith($name, 'module.') === false) {
+    $name = 'module.' . $name;
+  }
+
+  // Add to registry
+  $registry['blueprints']['pages/' . $name] = $blueprintArray;
+  $registry['templates'][$name] = $templatePath;
+  $registry['pageModels'][$name] = option('medienbaecker.modules.model', 'ModulePage');
+
+  return $registry;
+
 }
